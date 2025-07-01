e507470a43e37ef69679dacbe9a7a2de7d32f7eb
codedby-aadarsh <aadarshmandal9354@gmail.com>
2025-07-01 12:09:46 +0530

    Update mux4_1.v
    kinda blank

--- verilog_codes/mux4_1 project/mux4_1.v
+++ verilog_codes/mux4_1 project/mux4_1.v
@@ -1,1 +1,22 @@
-mux4_1.v
+// Code your design here
+module mux4_1(out,i0,i1,i2,i3,s0,s1);
+  //defining the i/o ports of 4:1 mux
+  output out;
+  input i0,i1,i2,i3,s0,s1;
+  
+  //internal wire declarations
+  wire s0n,s1n,y0,y1,y2,y3;
+  
+  not (s0n,s0);
+  not (s1n,s1);
+  
+  and (y0,i0,s1n,s0n);
+  and (y1,i1,s1n,s0);
+  and (y2,i2,s1,s0n);
+  and (y3,i3,s1,s0);
+  
+  or (out,y0,y1,y2,y3);
+endmodule;
+  
+  
+  
