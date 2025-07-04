10b48f645ddb9ea1182a7880a7ebfff46fd6b7d7
Aadarsh <aadarshmandal9354@gmail.com>
2025-07-04 10:49:16 +0530

    adding some new codes

--- verilog_codes/mux4_1 project/mux4_1.2.v
+++ verilog_codes/mux4_1 project/mux4_1.2.v
@@ -1,5 +1,6 @@
+//version 2 for implementing a 4 : 1 MUX by using the 
+//conditional operators
 module mux4_1(output out,
               input  s0,s1,i0,i1,i2,i3);
-              assign out = s1 ? (s0 ? i2 : i3):(s0 ? i1 : i0);
-            
+              assign out = s1 ? (s0 ? i2 : i3):(s0 ? i1 : i0);           
 endmodule
